1c28c482588b8a9d51623866889cc0f0e7a6f7f5
Kydyrgychov Arsen <kydyrgychov_a@IUCA.LOC>
2026-02-02 12:05:46 +0600

    feat: добавлена шапка в стиле BMW с логотипом и навигацией, стили вынесены в отдельный CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,48 @@
 import { useDispatch, useSelector } from "react-redux";
 import { toggleTheme } from "../features/ui/uiSlice";
-
+import "./Header.css";
 
 const Header = () => {
-    const dispatch = useDispatch()
-    const theme = useSelector(state => state.ui.theme)
+    const dispatch = useDispatch();
+    const theme = useSelector(state => state.ui.theme);
 
+    const navigation = [
+        "Автомобили",
+        "Электромобили",
+        "Онлайн магазин",
+        "Больше о BMW"
+    ];
 
-    return(
+    return (
+        <header className={`bmw-header ${theme}`}>
+            <div className="header-left">
+                {/* Заменяем иконку на изображение логотипа BMW */}
+                <div className="bmw-logo-container">
+                    <img 
+                        src="https://upload.wikimedia.org/wikipedia/commons/4/44/BMW.svg" 
+                        alt="BMW Logo" 
+                        className="bmw-logo-img"
+                    />
+                </div>
 
-        <header style={{ 
-            padding: '20px',
-            background: theme === 'light' ? "#eee": "#333",
-            color: theme === 'light' ? '#000' : "#fff"
-        }} >
-            <h1>Мой сайт</h1>
-            <button onClick={() => dispatch(toggleTheme())} >
-                Сменить тему
-            </button>
+                <nav>
+                    <ul className="nav-menu">
+                        {navigation.map((item, index) => (
+                            <li key={index} className="nav-link">
+                                {item}
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
 
-
+            <div className="header-right">
+                <button className="theme-btn" onClick={() => dispatch(toggleTheme())}>
+                    {theme === 'light' ? "🌙 Тёмная тема" : "☀️ Светлая тема"}
+                </button>
+            </div>
         </header>
-    )
-
-
+    );
 }
 
-export default Header
+export default Header;
